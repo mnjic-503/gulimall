8a0b93861f83009a4f41bfbe35a2912bc7eb0178
mnjic-503 <269810721@qq.com>
2020-04-17 17:16:02 +0800

    项目初始结构创建

---
+++ gulimall-coupin/src/test/java/com/atguigu/gulimall/coupin/GulimallCoupinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.coupin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCoupinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
